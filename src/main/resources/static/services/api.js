55e60596307759fe47ab0e25ef1d916b785e7f40
GuilhermeMonteiroLins <guilins777@gmail.com>
2023-06-10 23:17:46 -0300

    Create view add new films in list films

--- src/main/resources/static/services/api.js
+++ src/main/resources/static/services/api.js
@@ -1,19 +1,48 @@
-const url = "http://localhost:8080/film";
+function buscaDados(urlJson, params, jwt, accept = 'application/json') {
+	return new Promise(function(resolve, reject) {
+		const xhr = new XMLHttpRequest();
+		let urlAjustada = urlJson;
+		if (params) {
+			urlAjustada = urlAjustada + '?' + params
+		}
+		xhr.open('GET', urlJson, true);
+		xhr.setRequestHeader('Content-type', 'application/json');
+		xhr.setRequestHeader('Accept', accept);
+		if (jwt != null) {
+			xhr.setRequestHeader('Authorization', 'Bearer ' + jwt);
+		}
 
-export const apiGetAllFilms = async () => {
-    let data = [];
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				const dados = JSON.parse(xhr.responseText);
+				resolve(dados);
+			} else {
+				reject({
+					status: xhr.status,
+					mensagem: 'Erro na requisição'
+				});
+			}
+		};
+		xhr.send();
+	});
+}
 
-    await fetch(`${url}`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-      }
-    })
-      .then(async (response) => {
-        data = await response.json();
-      })
-      .catch(async (error) => {
-        console.log(error)
-      });
-    return data;
-  };
+function enviaDados(urlJson, dados) {
+	return new Promise(function(resolve, reject) {
+		let xhr = new XMLHttpRequest();
+		xhr.open('POST', urlJson, true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				resolve(JSON.parse(xhr.responseText));
+			} else {
+				reject({
+					codigo: xhr.status,
+					mensagem: 'Erro ao buscar dados'
+				});
+			}
+		};
+		xhr.send(JSON.stringify(dados));
+	});
+}
